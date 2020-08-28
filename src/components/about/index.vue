b16590104c61139ac2b13309d91c26c0b50aba52
Kyle Henderson <kthenderson83@gmail.com>
2020-08-27 17:15:17 -0700

    update about us content

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -1,5 +1,30 @@
 <template>
-	<div>Bid ol' about page where we talk about Recon Hunt.</div>
+	<v-container>
+		<v-row>
+			<v-col cols="12">
+				<span class="subheading">At Recon Hunt, we are true sportsmen with a passion for the outdoors.</span>
+			</v-col>
+			<v-col cols="12">
+				<span
+					class="subheading"
+				>The common challenge we all face is finding that perfect tree to set your deer stand, the undiscovered fishing hole, or the best guide to show you the ropes somewhere new. Whatever your outdoor passion may be, Recon Hunt has you covered.</span>
+			</v-col>
+			<v-col cols="12">
+				<span
+					class="subheading"
+				>Our mission at Recon Hunt is to create an online community that allows outdoorsmen to connect with landowners and outfitters to find that perfect experience. Our service allows users to easily view a large selection of properties and guided tours.</span>
+			</v-col>
+			<v-col cols="12">
+				<span
+					class="subheading"
+				>Once you've found your ideal experience, seamlessly connect with the landowner or outfitter and lock in your next adventure.</span>
+			</v-col>
+			<v-col cols="12">
+				<span class="subheading">Let the Hunt begin.</span>
+				<p class="title font-weight-regular">- Recon Hunt Team</p>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
